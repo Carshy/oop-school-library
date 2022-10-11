f5c9093b4ae6fe272b864a0739eba6f05aa5810d
Collins <mussocollins@gmail.com>
2022-10-11 18:33:04 +0300

    Fix linters

--- main.rb
+++ main.rb
@@ -1,12 +1,2 @@
 require './refactor'
 
-class Main
-  def start
-    puts 'Welcome to our School Library App'
-    app = Refactor.new
-    app.user_input
-  end 
-end
-
-main = Main.new
-main.start
